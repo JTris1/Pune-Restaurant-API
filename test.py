b136303cd39bc61d4383b9a3debc871296450be5
Johnathon Trisler <johnathontrisler@gmail.com>
2022-10-26 17:52:32 -0400

    Start of conditional for querystring

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import json
 import mimetypes
+import re
+from urllib import response
 from colorama import Fore, Style
 from pprint import pprint
 from flask import Flask, render_template, request, Response, url_for, redirect, jsonify
@@ -24,20 +26,32 @@ def index():
 
 @app.route('/restaurants')
 
+# def queryRestaurants():
+#     if 'name' in request.args:
+#         cursor = restaurants.find_one({'Restaurant_Name': request.args['name']})
+#         json_data = dumps(cursor)
+#         resp = Response(json_data, status=200, mimetype="application/json")
+#         return resp
+#     elif 'known_for_food' in request.args:
+#         cursor = restaurants.find({'Known_For_Food': request.args['known_for_food']})
+#         json_data = dumps(cursor)
+#         resp = Response(json_data, status=200, mimetype="application/json")
+#         return resp
+#     elif 'coordinates' in request.args:
+#         latitude = request.args.getlist('coordinates')[0]
+#         longitude = request.args.getlist('coordinates')[1]
+#         print(latitude)
+#         return Response(status=200)
+#     else:
+#         return Response(status=404)
+
 def queryRestaurants():
-    if 'name' in request.args:
-        cursor = restaurants.find_one({'Restaurant_Name': request.args['name']})
-        json_data = dumps(cursor)
-        resp = Response(json_data, status=200, mimetype="application/json")
-        return resp
-    elif 'known_for_food' in request.args:
-        cursor = restaurants.find({'Known_For_Food': request.args['known_for_food']})
-        json_data = dumps(cursor)
-        resp = Response(json_data, status=200, mimetype="application/json")
-        return resp
-    else:
-        cursor = restaurants.find()
-        list_cur = list(cursor)
-        json_data = dumps(list_cur)
-        resp = Response(json_data, status=200, mimetype="application/json")
-        return resp
+    req_list = request.args.to_dict(flat=False);
+    for key in req_list:
+        if(key == 'name'):
+            if(len(req_list['name']) > 1):
+                print("MULTIPLE NAMES, IMPLEMENT CODE HERE")
+            else:
+                print(req_list['name'][0])
+        # ADD CODE FOR ALL OTHER CONDITIONALS
+    return Response(status=200)

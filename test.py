10b89cf25b91cdbcf0b0c96a76c5302bffc5f232
JTris1 <johnathontrisler@gmail.com>
2022-10-31 11:29:54 -0400

    Added filters, name now case insensitive

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import json
 import mimetypes
 import re
-from urllib import response
 from colorama import Fore, Style
 from pprint import pprint
 from flask import Flask, render_template, request, Response, url_for, redirect, jsonify
@@ -15,8 +14,10 @@ app = Flask(__name__)
 
 client = MongoClient(config["ATLAS_URI"]);
 db = client.restaurant_api
+
 print(Fore.GREEN + "Connected to 'restaurant_api' database")
 
+# Assign database prefixes to variable
 restaurants = db.pune_restaurants
 neighborhoods = db.pune_neighborhoods
 
@@ -25,33 +26,39 @@ def index():
     return render_template('index.html')
 
 @app.route('/restaurants')
-
-# def queryRestaurants():
-#     if 'name' in request.args:
-#         cursor = restaurants.find_one({'Restaurant_Name': request.args['name']})
-#         json_data = dumps(cursor)
-#         resp = Response(json_data, status=200, mimetype="application/json")
-#         return resp
-#     elif 'known_for_food' in request.args:
-#         cursor = restaurants.find({'Known_For_Food': request.args['known_for_food']})
-#         json_data = dumps(cursor)
-#         resp = Response(json_data, status=200, mimetype="application/json")
-#         return resp
-#     elif 'coordinates' in request.args:
-#         latitude = request.args.getlist('coordinates')[0]
-#         longitude = request.args.getlist('coordinates')[1]
-#         print(latitude)
-#         return Response(status=200)
-#     else:
-#         return Response(status=404)
-
-def queryRestaurants():
+def get():
+    # Assings request args to a dictionary
     req_list = request.args.to_dict(flat=False);
+    filter_list = {}
+
     for key in req_list:
         if(key == 'name'):
+            # Make name case insensitive
+            re_name = re.compile(req_list['name'][0], re.IGNORECASE)
+            # Multiple Names specified
             if(len(req_list['name']) > 1):
                 print("MULTIPLE NAMES, IMPLEMENT CODE HERE")
+                # TRY TO ITERATE THROUGH LIST OF NAMES
+                # Got an error about cannot set expression in for loop
             else:
-                print(req_list['name'][0])
-        # ADD CODE FOR ALL OTHER CONDITIONALS
-    return Response(status=200)
+                filter_list['Restaurant_Name'] = re_name
+        elif(key == 'category'):
+            filter_list['Category'] = req_list['category'][0]
+        elif(key == 'location'):
+            # Split 'location' comma-seperated query into a list from a string 
+            lat_long = str(req_list['location'][0]).split(',')
+            filter_list['Latitude'] = float(lat_long[0])
+            filter_list['Longitude'] = float(lat_long[1])
+        elif(key == 'locality'):
+            neighborhood = req_list['locality'][0]
+            filter_list['Locality'] = re.compile('^' + neighborhood, re.IGNORECASE)
+
+    # IF filter_list IS EMPTY, RESPOND WITH 404
+    if(filter_list == {}):
+        return Response(status=404)
+
+    # Query data with filter list
+    cursor = restaurants.find(filter_list)
+    json_data = dumps(cursor)
+    resp = Response(json_data, status=200, mimetype="application/json")
+    return resp
